baee86053170eb9c64b9e1e716a77aa01495da4a
UnibadanSu <unibadansu@gmail.com>
2022-03-10 11:33:51 +0100

    configured admin panel

--- news/models.py
+++ news/models.py
@@ -22,4 +22,19 @@ class Tag(models.Model):
         return self.name
     
 
-class 
+class Post(models.Model):
+    class Meta:
+        ordering = ['-publish_date']
+
+    title = models.CharField(max_length=255, unique=True)
+    subtitle = models.CharField(max_length=255, blank=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    body = models.TextField()
+    meta_description = models.CharField(max_length=155, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    publish_date = models.DateTimeField(blank=True, null=True)
+    published = models.BooleanField(default=False)
+
+    author = models.ForeignKey(Profile, on_delete=models.PROTECT)
+    tags = models.ManyToManyField(Tag, blank=True)
