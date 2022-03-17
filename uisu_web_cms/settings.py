d86ed4571216311f7573589c902b5d84aabb2317
UnibadanSu <unibadansu@gmail.com>
2022-03-17 21:31:17 +0100

    allowed host

--- uisu_web_cms/settings.py
+++ uisu_web_cms/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://uisu-web-cms.herokuapp.com/']
+ALLOWED_HOSTS = ['.herokuapp.com',]
 
 
 # Application definition
